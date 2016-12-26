c0890305f3e1e0cab79ef26256f06e56fbc6cc0c
Jayan Zaman <jayanzaman@gmail.com>
2016-12-26 16:04:56 -0500

    First Commit

---
+++ assets/js/init.js
@@ -0,0 +1,13 @@
+(function($){
+  $(function(){
+    $('.collapsible').collapsible();
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+  // $(document).ready(function(){
+  //   $('.collapsible').collapsible();
+  // });
+
